512e2a4533cb4729ee2d3e435c78cc17940c07f0
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2022-07-15 15:59:19 -0300

    feat: add values

--- app/javascript/controllers/sweetalert_controller.js
+++ app/javascript/controllers/sweetalert_controller.js
@@ -2,16 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 import Swal from "sweetalert2"
 // Connects to data-controller="sweetalert"
 export default class extends Controller {
+  static values = { title: String }
+  
   connect() {
-    console.log("Sweet")
+    this.redirect = false
   }
   showAlert (event) {
+    if (this.redirect) return
+
     event.stopImmediatePropagation()
     event.preventDefault()
-    Swal.fire(
-        'Good job!',
-        'You clicked the button!',
-        'success'
-      )
-    }  
+    
+    Swal.fire({
+      title: this.titleValue,
+      showCancelButton: true,
+      confirmButtonText: 'Go',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.redirect = true
+        this.element.click()
+      }
+    })    
+  
+  }
 }
