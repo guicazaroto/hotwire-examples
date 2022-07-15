77d969b00df098649eb9a4847dd3864decc0faec
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2022-07-15 17:14:40 -0300

    use stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = ['name', 'output']
+  // connect() {
+  //   this.element.textContent = "Hello World!"
+  // }
+
+  greet() {
+    this.outputTarget.textContent = `Hello ${this.nameTarget.value}!`
   }
 }
