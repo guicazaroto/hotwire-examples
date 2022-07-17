f40c664ca10446efd026bc72bdd71fd5b48d31b2
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2022-07-17 19:00:32 -0300

    table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if params[:query].present?
+      @users = User.where("name like? or email like?", "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      @users = User.all
+    end
+
+    respond_to do |format|
+      if turbo_frame_request? && turbo_frame_request_id == "search"
+        format.html { render partial: "users_table", locals: { users: @users } }
+      else
+        format.html
+      end
+    end
   end
 end
