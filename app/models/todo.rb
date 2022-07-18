718d1d9a61798d2d939fccdf53bc627444e0e39c
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2022-07-18 13:50:25 -0300

    add remove broadcast

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,6 @@
 class Todo < ApplicationRecord
   broadcasts
+  after_destroy_commit { broadcast_remove_to "todos" }
   after_update_commit { broadcast_append_to "todos" }
   validates :title, presence: true
   enum status: { incomplete: 0, complete: 1 }
